cd6bafd286a4a25646f658d831cc7356ccaeab88
wuxw <tankwoo@icloud.com>
2021-03-19 11:25:59 +0800

    generate maven skeleton

---
+++ practice/src/main/java/com/leetcode/PracticeApp.java
@@ -0,0 +1,13 @@
+package com.leetcode;
+
+/**
+ * Hello world!
+ *
+ */
+public class PracticeApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
