daad2e93d9f5532cf11b7faf8aa661007d6eb1e7
wuxw <tankwoo@icloud.com>
2021-03-20 23:57:06 +0800

    add 1137

---
+++ practice/src/main/java/com/leetcode/problems/p1137/TestSolution.java
@@ -0,0 +1,16 @@
+package com.leetcode.problems.p1137;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @Author: woosang
+ * @Date: 2021-03-20 11:53 PM
+ * @Version: 1.0
+ */
+public class TestSolution {
+    @Test
+    public void testTribonacci(){
+        Assert.assertEquals(7,new Solution().tribonacci(5));
+    }
+}
