dedb727df996c0ccc4873d9e72f5850f549ef5ae
wuxw <tankwoo@icloud.com>
2021-03-22 12:56:39 +0800

    add 104

---
+++ practice/src/main/java/com/leetcode/problems/p104/Solution.java
@@ -0,0 +1,30 @@
+package com.leetcode.problems.p104;
+
+/**
+ * @Author: woosang
+ * @Date: 2021-03-22 12:55 PM
+ * @Version: 1.0
+ * Given the root of a binary tree, return its maximum depth.
+ *
+ * A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: 3
+ * Example 2:
+ *
+ * Input: root = [1,null,2]
+ * Output: 2
+ * Example 3:
+ *
+ * Input: root = []
+ * Output: 0
+ * Example 4:
+ *
+ * Input: root = [0]
+ * Output: 1
+ */
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null) return 0;
+        return Math.max(maxDepth(root.left),maxDepth(root.right))+1;
+    }
+}
---
+++ practice/src/main/java/com/leetcode/problems/p104/TreeNode.java
@@ -0,0 +1,25 @@
+package com.leetcode.problems.p104;
+
+/**
+ * @Author: woosang
+ * @Date: 2021-03-19 11:14 PM
+ * @Version: 1.0
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
